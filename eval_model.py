dc4dd1c56a088cf6c583a8c27957615d7bd6a770
Aldo <amarinim@andrew.cmu.edu>
2018-10-01 19:24:47 -0400

    Fix make_pred_multilabel

--- eval_model.py
+++ eval_model.py
@@ -1,17 +1,8 @@
-import torch
-import pandas as pd
-import cxr_dataset as CXR
-from torchvision import transforms, utils
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-import sklearn
-import sklearn.metrics as sklm
 from torch.autograd import Variable
 import numpy as np
-import os
 
 
-def make_pred_multilabel(dataloader, model, nih_labels):
+def make_pred_multilabel(dataloader, model):
     """
     Gives predictions for test fold and calculates AUCs using previously trained model
 
@@ -26,77 +17,21 @@ def make_pred_multilabel(dataloader, model, nih_labels):
     # set model to eval mode; required for proper predictions given use of batchnorm
     model.train(False)
 
-    # create empty dfs
-    pred_df = pd.DataFrame(columns=["Image Index"])
-    true_df = pd.DataFrame(columns=["Image Index"])
+    preds = []
+    obs = []
 
     # iterate over dataloader
     for i, data in enumerate(dataloader):
 
         inputs, labels, _ = data
-        inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-
-        true_labels = labels.cpu().data.numpy()
-        batch_size = true_labels.shape[0]
+        obs.append(labels.cpu().numpy())
+        inputs = Variable(inputs.cuda())
 
         outputs = model(inputs)
         probs = outputs.cpu().data.numpy()
-        
-        # TODO: fix this mess (what does it even do?)
-        # get predictions and true values for each item in batch
-        for j in range(0, batch_size):
-            thisrow = {}
-            truerow = {}
-            thisrow["Image Index"] = dataloader.dataset.df.index[batch_size * i + j]
-            truerow["Image Index"] = dataloader.dataset.df.index[batch_size * i + j]
+        pred = np.argmax(probs, axis=1)
+        preds.append(pred)
 
-            # iterate over each entry in prediction vector; each corresponds to
-            # individual label
-            for k in range(len(dataloader.dataset.PRED_LABEL)):
-                thisrow["prob_" + dataloader.dataset.PRED_LABEL[k]] = probs[j, k]
-                truerow[dataloader.dataset.PRED_LABEL[k]] = true_labels[j, k]
-
-            pred_df = pred_df.append(thisrow, ignore_index=True)
-            true_df = true_df.append(truerow, ignore_index=True)
-
-        if(i % 10 == 0):
-            print(str(i * batch_size))
-
-    auc_df = pd.DataFrame(columns=["label", "auc"])
-
-    # calc AUCs
-    for column in true_df:
-
-        if column not in [
-            'Atelectasis',
-            'Cardiomegaly',
-            'Effusion',
-            'Infiltration',
-            'Mass',
-            'Nodule',
-            'Pneumonia',
-            'Pneumothorax',
-            'Consolidation',
-            'Edema',
-            'Emphysema',
-            'Fibrosis',
-            'Pleural_Thickening',
-                'Hernia']:
-                    continue
-        actual = true_df[column]
-        pred = pred_df["prob_" + column]
-        thisrow = {}
-        thisrow['label'] = column
-        thisrow['auc'] = np.nan
-        try:
-            thisrow['auc'] = sklm.roc_auc_score(
-                actual.as_matrix().astype(int), pred.as_matrix())
-        except BaseException:
-            print("can't calculate auc for " + str(column))
-        auc_df = auc_df.append(thisrow, ignore_index=True)
-    
-    if not os.path.exists('results'):
-        os.makedirs('results')
-    pred_df.to_csv("results/preds.csv", index=False)
-    auc_df.to_csv("results/aucs.csv", index=False)
-    return pred_df, true_df, auc_df
+    preds = np.concatenate(preds, axis=0)
+    obs = np.concatenate(obs)
+    return preds, obs
